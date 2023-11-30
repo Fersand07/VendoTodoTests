ec25bb7693dd6e28201b4da4aadd6b0724b4a4a7
Fosand11 <dev.fsand@gmail.com>
2023-11-30 00:12:33 -0600

    login & logout

--- REACT PROJECT/src/routes/auth.routes.js
+++ REACT PROJECT/src/routes/auth.routes.js
@@ -3,7 +3,10 @@ import {register, login} from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', register);
+
+router.post('/login', login);
+
+router.post('/logout', login);
 
 export default router
